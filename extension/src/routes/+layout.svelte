beca32b4f668982b7239639c05f3de80b224761c
Ben Juntilla <46634820+benjuntilla@users.noreply.github.com>
2023-10-09 12:49:42 -0700

    add chat interface

--- extension/src/routes/+layout.svelte
+++ extension/src/routes/+layout.svelte
@@ -1,4 +1,4 @@
-<script lang='ts'>
+<script lang="ts">
 	// The ordering of these imports is critical to your app working properly
 	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
 	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
@@ -6,6 +6,7 @@
 	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
 	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import image from '../static/logo.png';
 </script>
 
 <!-- App Shell -->
@@ -14,10 +15,15 @@
 		<!-- App Bar -->
 		<AppBar>
 			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Study Sphere</strong>
+				<img class="object-contain h-16" src={image} alt="" />
+				<h2 class="h2">
+					<span
+						class="bg-gradient-to-br from-red-500 to-yellow-500 bg-clip-text text-transparent box-decoration-clone"
+						>Study Sphere</span
+					>
+				</h2>
 			</svelte:fragment>
-			<svelte:fragment slot="trail">
-			</svelte:fragment>
+			<svelte:fragment slot="trail" />
 		</AppBar>
 	</svelte:fragment>
 	<!-- Page Route Content -->
