5c4abfa2205ac4ec6c8ab781bcfcf9a3d34861b6
Ben Juntilla <46634820+benjuntilla@users.noreply.github.com>
2023-10-10 13:46:26 -0700

    add sidebar to chat

--- website/src/lib/components/Chat.svelte
+++ website/src/lib/components/Chat.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	import { faker } from '@faker-js/faker';
 	import { onMount } from 'svelte';
-	import { postQuery } from '$lib/intermediary';
+	import { getText } from '$lib/intermediary';
+	import { ListBox, ListBoxItem } from '@skeletonlabs/skeleton';
 
 	const lorem = faker.lorem.paragraph();
 	let currentMessage = '';
@@ -44,7 +45,7 @@
 	}
 
 	function addMessage(): void {
-		postQuery(currentMessage);
+		getText(currentMessage);
 		const newMessage = {
 			id: messageFeed.length,
 			host: true,
@@ -72,56 +73,89 @@
 	onMount(() => {
 		scrollChatBottom();
 	});
+
+	interface Subject {
+		id: number;
+		name: string;
+	}
+	// Navigation List
+	const subjects: Subject[] = [
+		{ id: 0, name: 'Michael' },
+		{ id: 1, name: 'Janet' },
+		{ id: 2, name: 'Susan' },
+		{ id: 3, name: 'Joey' },
+		{ id: 4, name: 'Lara' },
+		{ id: 5, name: 'Melissa' }
+	];
+	let currentSubject: Subject = subjects[0];
+	let valueSingle = '';
 </script>
 
 <section class="card">
-	<div class="grid grid-row-[1fr_auto]">
-		<!-- Conversation -->
-		<section bind:this={elemChat} class="max-h-[500px] p-4 overflow-y-auto space-y-4">
-			{#each messageFeed as bubble}
-				{#if bubble.host === true}
-					<div class="grid grid-cols-[auto_1fr] gap-2">
-						<div class="card p-4 variant-soft rounded-tl-none space-y-2">
-							<header class="flex justify-between items-center">
-								<p class="font-bold">Me</p>
-							</header>
-							<p>{bubble.message}</p>
-						</div>
-					</div>
-				{:else}
-					<div class="grid grid-cols-[1fr_auto] gap-2">
-						<div class="card p-4 rounded-tr-none space-y-2 variant-soft-primary">
-							<header class="flex justify-between items-center">
-								<p class="font-bold">Sia</p>
-							</header>
-							<p>{bubble.message}</p>
-						</div>
-					</div>
-				{/if}
-			{/each}
-		</section>
-		<!-- Prompt -->
-		<section class="border-t border-surface-500/30 p-4">
-			<div
-				class="input-group input-group-divider grid-cols-[auto_1fr_auto] rounded-container-token"
-			>
-				<button class="input-group-shim">+</button>
-				<textarea
-					bind:value={currentMessage}
-					class="bg-transparent border-0 ring-0"
-					name="prompt"
-					id="prompt"
-					placeholder="Write a message..."
-					rows="1"
-					on:keydown={onPromptKeydown}
-				/>
-				<button
-					class={currentMessage ? 'variant-filled-primary' : 'input-group-shim'}
-					on:click={addMessage}
-				>
-					<i class="fa-solid fa-paper-plane" />
-				</button>
+	<div class="chat w-full h-full grid grid-cols-1 lg:grid-cols-[30%_1fr]">
+		<!-- Navigation -->
+		<div class="hidden lg:grid grid-rows-[auto_1fr_auto] border-r border-surface-500/30">
+			<!-- List -->
+			<div class="p-4 space-y-4 overflow-y-auto">
+				<small class="opacity-50">Subjects</small>
+				<ListBox active="variant-filled-primary">
+					{#each subjects as subject}
+						<ListBoxItem bind:group={currentSubject} name="subjects" value={subject}>
+							{subject.name}
+						</ListBoxItem>
+					{/each}
+				</ListBox>
 			</div>
-		</section>
+		</div>
+		<!-- Chat -->
+		<div class="grid grid-row-[1fr_auto]">
+			<!-- Conversation -->
+			<section bind:this={elemChat} class="max-h-[500px] p-4 overflow-y-auto space-y-4">
+				{#each messageFeed as bubble}
+					{#if bubble.host === true}
+						<div class="grid grid-cols-[auto_1fr] gap-2">
+							<div class="card p-4 variant-soft rounded-tl-none space-y-2">
+								<header class="flex justify-between items-center">
+									<p class="font-bold">Me</p>
+								</header>
+								<p>{bubble.message}</p>
+							</div>
+						</div>
+					{:else}
+						<div class="grid grid-cols-[1fr_auto] gap-2">
+							<div class="card p-4 rounded-tr-none space-y-2 variant-soft-primary">
+								<header class="flex justify-between items-center">
+									<p class="font-bold">Sia</p>
+								</header>
+								<p>{bubble.message}</p>
+							</div>
+						</div>
+					{/if}
+				{/each}
+			</section>
+			<!-- Prompt -->
+			<section class="border-t border-surface-500/30 p-4">
+				<div
+					class="input-group input-group-divider grid-cols-[auto_1fr_auto] rounded-container-token"
+				>
+					<button class="input-group-shim">+</button>
+					<textarea
+						bind:value={currentMessage}
+						class="bg-transparent border-0 ring-0"
+						name="prompt"
+						id="prompt"
+						placeholder="Write a message..."
+						rows="1"
+						on:keydown={onPromptKeydown}
+					/>
+					<button
+						class={currentMessage ? 'variant-filled-primary' : 'input-group-shim'}
+						on:click={addMessage}
+					>
+						<i class="fa-solid fa-paper-plane" />
+					</button>
+				</div>
+			</section>
+		</div>
 	</div>
 </section>
