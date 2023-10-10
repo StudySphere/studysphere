3f706d25769260bd3217b93ea15339397ba5aa29
densumesh <dens.sumesh79@gmail.com>
2023-10-10 09:25:01 -0700

    feature: auth flow for google and arguflow

--- parser-backend/application.py
+++ parser-backend/application.py
@@ -1,189 +1,268 @@
 # -*- coding: utf-8 -*-
 
 import os
+from threading import Thread
 import flask
 import requests
 import io
 import spacy
+from tika import parser as p
+import json
+
+
 # Load the installed model "en_core_web_sm"
 nlp = spacy.load("en_core_web_sm")
 import google.oauth2.credentials
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
+import requests
 
 # This variable specifies the name of a file that contains the OAuth 2.0
 # information for this application, including its client_id and client_secret.
-CLIENT_SECRETS_FILE = "/Users/denssumesh/Documents/GitHub/extension/parser-backend/client_secret.json"
+CLIENT_SECRETS_FILE = (
+    "/Users/denssumesh/Documents/GitHub/extension/parser-backend/client_secret.json"
+)
 
 # This OAuth 2.0 access scope allows for full read/write access to the
 # authenticated user's account and requires requests to use an SSL connection.
-SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
-API_SERVICE_NAME = 'drive'
-API_VERSION = 'v3'
+SCOPES = ["https://www.googleapis.com/auth/drive.readonly"]
+API_SERVICE_NAME = "drive"
+API_VERSION = "v3"
 
 app = flask.Flask(__name__)
 # Note: A secret key is included in the sample so that it works.
 # If you use this code in your application, replace this with a truly secret
 # key. See https://flask.palletsprojects.com/quickstart/#sessions.
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+app.SESSION_COOKIE_HTTPONLY = False
 
-@app.route('/')
-def index():
-    return print_index_table()
 
-@app.route('/test')
-def test_api_request():
-    if 'credentials' not in flask.session:
-        return flask.redirect('authorize')
+def upload_data(drive, filesIds):
+    for file_id in filesIds:
+        file = drive.files().get(fileId=file_id).execute()
+        if (
+            file["mimeType"] == "application/vnd.google-apps.document"
+            or file["mimeType"] == "application/vnd.google-apps.presentation"
+        ):
+            request = drive.files().export_media(fileId=file_id, mimeType="text/plain")
+            file = io.BytesIO(request.execute())
+            file.seek(0)
+            docfile = file.read().decode("utf-8")
+            doc = nlp(docfile)
+            combined_list = [
+                doc[i].text + " " + doc[i + 1].text
+                for i in range(0, len(list(doc.sents)), 20)
+            ]
+
+            body = {
+                "cardHtml": chunk,
+                "link": file["webViewLink"],
+                "tags": [file["mimeType"]],
+                "private": True,
+            }
+
+            for chunk in combined_list:
+                requests.post(
+                    "https://studysphere-api.arguflow.ai/api/v1/card",
+                    json=body,
+                    cookies=flask.request.cookies.get("arguflow_credentials"),
+                )
+        else:
+            request = drive.files().get_media(fileId=file_id)
+            file = io.BytesIO(request.execute())
+            binaryFile = p.from_buffer(file)
+            docfile = binaryFile["content"].strip()
+            doc = nlp(docfile)
+            combined_list = [
+                doc[i].text + " " + doc[i + 1].text
+                for i in range(0, len(list(doc.sents)), 20)
+            ]
+            body = {
+                "cardHtml": chunk,
+                "link": file["webViewLink"],
+                "tags": [file["mimeType"]],
+                "private": True,
+            }
+
+            for chunk in combined_list:
+                requests.post(
+                    "https://studysphere-api.arguflow.ai/api/v1/card",
+                    json=body,
+                    cookies=flask.request.cookies.get("arguflow_credentials"),
+                )
+
+
+@app.route("/upload_gdrive", methods=["POST"])
+def upload_gdrive():
+    if "google_credentials" not in flask.session:
+        return flask.make_response("User is not google authed", 401)
+    if "arguflow_credentials" not in flask.session:
+        return flask.make_response("User is not arguflow authed", 401)
 
     # Load credentials from the session.
     credentials = google.oauth2.credentials.Credentials(
-        **flask.session['credentials'])
+        **flask.session["google_credentials"]
+    )
 
     drive = googleapiclient.discovery.build(
-        API_SERVICE_NAME, API_VERSION, credentials=credentials)
+        API_SERVICE_NAME, API_VERSION, credentials=credentials
+    )
 
-    files = drive.files().list(
-        q="mimeType='application/vnd.google-apps.document'",
-            spaces='drive',
-            fields='nextPageToken, '
-            'files(id, name)',
-    ).execute()
+    fileIds = flask.request.json()["filesIds"]
+    Thread(target=upload_data, args=(drive, fileIds)).start()
 
-    filesIds = [file["id"] for file in files['files']]
-    for file_id in filesIds:
-        request = drive.files().export_media(fileId=file_id,
-                                            mimeType='text/plain')
-        file = io.BytesIO(request.execute())
-        file.seek(0)
-        docfile = file.read().decode("utf-8")
-        doc = nlp(docfile)
-        combined_list = [doc[i].text + " " + doc[i + 1].text
-                         for i in range(0, len(list(doc.sents)), 20)]
-        print(combined_list)
-        return flask.make_response(combined_list)
+    return flask.make_response("Success", 200)
 
-    # Save credentials back to session in case access token was refreshed.
+
+@app.route("/authorize")
+def authorize():
+    # Create flow instance to manage the OAuth 2.0 Authorization Grant Flow steps.
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+        CLIENT_SECRETS_FILE, scopes=SCOPES
+    )
+
+    # The URI created here must exactly match one of the authorized redirect URIs
+    # for the OAuth 2.0 client, which you configured in the API Console. If this
+    # value doesn't match an authorized URI, you will get a 'redirect_uri_mismatch'
+    # error.
+    flow.redirect_uri = flask.url_for("oauth2callback", _external=True)
+
+    authorization_url, state = flow.authorization_url(
+        # Enable offline access so that you can refresh an access token without
+        # re-prompting the user for permission. Recommended for web server apps.
+        access_type="offline",
+        # Enable incremental authorization. Recommended as a best practice.
+        include_granted_scopes="true",
+    )
+
+    # Store the state so the callback can verify the auth server response.
+    flask.session["state"] = state
+
+    return flask.redirect(authorization_url)
+
+
+@app.route("/oauth2callback")
+def oauth2callback():
+    # Specify the state when creating the flow in the callback so that it can
+    # verified in the authorization server response.
+    state = flask.session["state"]
+
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+        CLIENT_SECRETS_FILE, scopes=SCOPES, state=state
+    )
+    flow.redirect_uri = flask.url_for("oauth2callback", _external=True)
+
+    # Use the authorization server's response to fetch the OAuth 2.0 tokens.
+    authorization_response = flask.request.url
+    flow.fetch_token(authorization_response=authorization_response)
+
+    # Store credentials in the session.
     # ACTION ITEM: In a production app, you likely want to save these
     #              credentials in a persistent database instead.
-    flask.session['credentials'] = credentials_to_dict(credentials)
+    credentials = flow.credentials
+    flask.session["credentials"] = credentials_to_dict(credentials)
+    resp = flask.make_response("Success", 200)
+    resp.set_cookie("google_credentials", json.dumps(credentials_to_dict(credentials)))
+    return resp
 
 
-
-
-@app.route('/authorize')
-def authorize():
-  # Create flow instance to manage the OAuth 2.0 Authorization Grant Flow steps.
-  flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-      CLIENT_SECRETS_FILE, scopes=SCOPES)
-
-  # The URI created here must exactly match one of the authorized redirect URIs
-  # for the OAuth 2.0 client, which you configured in the API Console. If this
-  # value doesn't match an authorized URI, you will get a 'redirect_uri_mismatch'
-  # error.
-  flow.redirect_uri = flask.url_for('oauth2callback', _external=True)
-
-  authorization_url, state = flow.authorization_url(
-      # Enable offline access so that you can refresh an access token without
-      # re-prompting the user for permission. Recommended for web server apps.
-      access_type='offline',
-      # Enable incremental authorization. Recommended as a best practice.
-      include_granted_scopes='true')
-
-  # Store the state so the callback can verify the auth server response.
-  flask.session['state'] = state
-
-  return flask.redirect(authorization_url)
-
-
-@app.route('/oauth2callback')
-def oauth2callback():
-  # Specify the state when creating the flow in the callback so that it can
-  # verified in the authorization server response.
-  state = flask.session['state']
-
-  flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-      CLIENT_SECRETS_FILE, scopes=SCOPES, state=state)
-  flow.redirect_uri = flask.url_for('oauth2callback', _external=True)
-
-  # Use the authorization server's response to fetch the OAuth 2.0 tokens.
-  authorization_response = flask.request.url
-  flow.fetch_token(authorization_response=authorization_response)
-
-  # Store credentials in the session.
-  # ACTION ITEM: In a production app, you likely want to save these
-  #              credentials in a persistent database instead.
-  credentials = flow.credentials
-  flask.session['credentials'] = credentials_to_dict(credentials)
-
-  return flask.redirect(flask.url_for('test_api_request'))
-
-
-@app.route('/revoke')
+@app.route("/revoke")
 def revoke():
-  if 'credentials' not in flask.session:
-    return ('You need to <a href="/authorize">authorize</a> before ' +
-            'testing the code to revoke credentials.')
+    if "credentials" not in flask.session:
+        return (
+            'You need to <a href="/authorize">authorize</a> before '
+            + "testing the code to revoke credentials."
+        )
 
-  credentials = google.oauth2.credentials.Credentials(
-      **flask.session['credentials'])
+    credentials = google.oauth2.credentials.Credentials(**flask.session["credentials"])
 
-  revoke = requests.post('https://oauth2.googleapis.com/revoke',
-                         params={'token': credentials.token},
-                         headers={'content-type': 'application/x-www-form-urlencoded'})
+    revoke = requests.post(
+        "https://oauth2.googleapis.com/revoke",
+        params={"token": credentials.token},
+        headers={"content-type": "application/x-www-form-urlencoded"},
+    )
 
-  status_code = getattr(revoke, 'status_code')
-  if status_code == 200:
-    return('Credentials successfully revoked.' + print_index_table())
-  else:
-    return('An error occurred.' + print_index_table())
+    status_code = getattr(revoke, "status_code")
+    if status_code == 200:
+        return "Credentials successfully revoked."
+    else:
+        return "An error occurred."
 
 
-@app.route('/clear')
+@app.route("/clear")
 def clear_credentials():
-  if 'credentials' in flask.session:
-    del flask.session['credentials']
-  return ('Credentials have been cleared.<br><br>' +
-          print_index_table())
+    if "credentials" in flask.session:
+        del flask.session["credentials"]
+    return "Credentials have been cleared.<br><br>"
 
 
 def credentials_to_dict(credentials):
-  return {'token': credentials.token,
-          'refresh_token': credentials.refresh_token,
-          'token_uri': credentials.token_uri,
-          'client_id': credentials.client_id,
-          'client_secret': credentials.client_secret,
-          'scopes': credentials.scopes}
+    return {
+        "token": credentials.token,
+        "refresh_token": credentials.refresh_token,
+        "token_uri": credentials.token_uri,
+        "client_id": credentials.client_id,
+        "client_secret": credentials.client_secret,
+        "scopes": credentials.scopes,
+    }
 
 
-def print_index_table():
-  return ('<table>' +
-          '<tr><td><a href="/test">Test an API request</a></td>' +
-          '<td>Submit an API request and see a formatted JSON response. ' +
-          '    Go through the authorization flow if there are no stored ' +
-          '    credentials for the user.</td></tr>' +
-          '<tr><td><a href="/authorize">Test the auth flow directly</a></td>' +
-          '<td>Go directly to the authorization flow. If there are stored ' +
-          '    credentials, you still might not be prompted to reauthorize ' +
-          '    the application.</td></tr>' +
-          '<tr><td><a href="/revoke">Revoke current credentials</a></td>' +
-          '<td>Revoke the access token associated with the current user ' +
-          '    session. After revoking credentials, if you go to the test ' +
-          '    page, you should see an <code>invalid_grant</code> error.' +
-          '</td></tr>' +
-          '<tr><td><a href="/clear">Clear Flask session credentials</a></td>' +
-          '<td>Clear the access token currently stored in the user session. ' +
-          '    After clearing the token, if you <a href="/test">test the ' +
-          '    API request</a> again, you should go back to the auth flow.' +
-          '</td></tr></table>')
+@app.route("/arguflow_register", methods=["POST"])
+def register():
+    body = flask.request.json
+    r = requests.post(
+        "https://studysphere-api.arguflow.ai/api/invitation",
+        json={"email": body["email"], "referral_tokens": []},
+        headers={
+            "Content-Type": "application/json",
+            "Origin": "https://studysphere.ai",
+        },
+    )
+    if r.ok:
+        invUrl = r.json()["registration_url"]
+        invToken = invUrl.split("/")[-1].split("?")[0]
+        res = requests.post(
+            "https://studysphere-api.arguflow.ai/api/register/" + invToken,
+            json={
+                "password": body["password"],
+                "password_confirmation": body["password"],
+            },
+        )
+        if res.ok:
+            req = requests.post(
+                "https://studysphere-api.arguflow.ai/api/auth",
+                json={"email": body["email"], "password": body["password"]},
+            )
+            if req.ok:
+                resp = flask.make_response("Success", 200)
+                resp.set_cookie("arguflow_credentials", req.cookies["vault"])
+                return resp
+        else:
+            return flask.make_response("Failed", 500)
+    else:
+        return flask.make_response("Failed", 500)
 
 
-if __name__ == '__main__':
-  # When running locally, disable OAuthlib's HTTPs verification.
-  # ACTION ITEM for developers:
-  #     When running in production *do not* leave this option enabled.
-  os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+@app.route("/arguflow_login", methods=["POST"])
+def login():
+    body = flask.request.json
+    req = requests.post(
+        "https://studysphere-api.arguflow.ai/api/auth",
+        json={"email": body["email"], "password": body["password"]},
+    )
+    print(req.text)
+    if req.ok:
+        resp = flask.make_response("Success", 200)
+        resp.set_cookie("arguflow_credentials", req.cookies["vault"])
+        return resp
 
-  # Specify a hostname and port that are set as a valid redirect URI
-  # for your API project in the Google API Console.
-  app.run('localhost', 8080, debug=True)
+
+if __name__ == "__main__":
+    # When running locally, disable OAuthlib's HTTPs verification.
+    # ACTION ITEM for developers:
+    #     When running in production *do not* leave this option enabled.
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+    # Specify a hostname and port that are set as a valid redirect URI
+    # for your API project in the Google API Console.
+    app.run("localhost", 8080, debug=True)
