a748adc8137062705885c0ac9d9e572354ad77dc
densumesh <dens.sumesh79@gmail.com>
2023-10-10 11:10:21 -0700

    feature: change url

--- parser-backend/application.py
+++ parser-backend/application.py
@@ -123,7 +123,7 @@ def authorize():
     # for the OAuth 2.0 client, which you configured in the API Console. If this
     # value doesn't match an authorized URI, you will get a 'redirect_uri_mismatch'
     # error.
-    flow.redirect_uri = flask.url_for("oauth2callback", _external=True)
+    flow.redirect_uri = "https://studysphere-parser.arguflow.ai/oauth2callback"
 
     authorization_url, state = flow.authorization_url(
         # Enable offline access so that you can refresh an access token without
