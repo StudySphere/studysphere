325b3e51b4425ae6377c21718b04a8dff24c94fa
Ben Juntilla <46634820+benjuntilla@users.noreply.github.com>
2023-10-10 16:35:43 -0700

    change branding

--- chat/src/routes/index.tsx
+++ chat/src/routes/index.tsx
@@ -76,40 +76,19 @@ export default function Home() {
           <div class="hidden items-center sm:flex">
             <img
               class="w-10"
-              src="/logo_transparent.svg"
+              src="/logo.png"
               alt="Logo"
               elementtiming={""}
               fetchpriority={"high"}
             />
             <div>
-              <div class="mb-[-4px] w-full text-end align-bottom text-xs leading-3 text-turquoise">
-                {dataset}
-              </div>
               <div class="align-top text-lg">
-                <span>Arguflow</span>
-                <span class="text-magenta">Chat</span>\
+                <span>Study</span>
+                <span class="text-magenta">Sphere</span>\
               </div>
             </div>
           </div>
           <div class="flex items-center gap-4">
-            <div class="hidden items-center gap-4 md:flex">
-              <a class="hover:underline" href={searchURL}>
-                Search
-              </a>
-              <a class="hover:underline" href="https://blog.arguflow.ai/">
-                Blog
-              </a>
-            </div>
-            <Show when={showGithubStars !== "off"}>
-              <a href="https://github.com/arguflow/arguflow">
-                <div class="flex items-center justify-center rounded border border-black px-2 py-1 hover:border-gray-300 hover:bg-gray-300 dark:border-white dark:hover:border-neutral-700 dark:hover:bg-neutral-700">
-                  <AiFillGithub class="mr-2 h-[26px] w-[26px] fill-current" />
-                  <p class="text-sm">STAR US</p>
-                  <TbMinusVertical size={25} />
-                  <p>{starCount()}</p>
-                </div>
-              </a>
-            </Show>
             <ThemeModeController />
             <A
               class="rounded-lg bg-turquoise px-4 py-2 font-semibold dark:text-neutral-900"
@@ -122,81 +101,35 @@ export default function Home() {
         <div class="py-4" />
         <div class="flex flex-col items-center space-y-8">
           <div>
-            <div class="mb-[-4px] w-full text-end align-bottom text-lg leading-3 text-turquoise">
-              {dataset}
-            </div>
             <div class="text-5xl md:text-6xl">
-              <span>Arguflow</span>
-              <span class="text-magenta">Chat</span>
+              <span>Study</span>
+              <span class="text-magenta">Sphere</span>
             </div>
           </div>
           <p class="text-center text-lg">
-            Retrieval augmented LLM chatbot that lets you chat with{" "}
-            <span class="text-turquoise"> {dataset}</span>
+            Hey, I'm Sia! I supercharge your notes with helpful questions and
+            insights.
           </p>
           <A
             class="rounded-lg bg-turquoise px-4 py-2 font-semibold text-black shadow-md"
-            href={"https://arguflow.ai/meet"}
+            href={"/chat"}
           >
-            Get a Custom Solution
+            Chat with me!
           </A>
         </div>
       </div>
-      <Show when={youtubeEmbedURL}>
-        <div class="py-4" />
-        <div class="flex w-full justify-center">
-          <div class="w-fit px-4">
-            <iframe
-              class="h-[169px] w-[300px] md:h-[315px] md:w-[560px]"
-              src={youtubeEmbedURL}
-              title="YouTube video player"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              allowfullscreen
-            />
-          </div>
-        </div>
-      </Show>
       <div class="flex-1" />
       <footer class="mt-14 flex flex-col items-center bg-gradient-radial-b from-magenta pb-4 pt-20">
         <div class="flex items-center">
           <img
             class="w-14"
-            src="/logo_transparent.svg"
+            src="/logo.png"
             alt=""
             elementtiming={""}
             fetchpriority={"high"}
           />
-          <div>
-            <div class="mb-[-4px] w-full text-end align-bottom text-xs leading-3 text-turquoise">
-              {dataset}
-            </div>
-            <div class="align-top text-lg">
-              <span>Arguflow</span>
-              <span class="text-magenta">Chat</span>\
-            </div>
-          </div>
-        </div>
-        <div class="flex w-full flex-col  items-center gap-2">
-          <a href="mailto:contact@arguflow.gg">contact@arguflow.gg</a>
         </div>
         <div class="py-2" />
-        <div class="flex gap-3">
-          <a href="https://twitter.com/arguflowai" target="_blank">
-            <BiLogosTwitter size={30} class="fill-current" />
-          </a>
-          <a href="https://twitch.tv/arguflow" target="_blank">
-            <BiLogosTwitch size={30} class="fill-current" />
-          </a>
-          <a href="https://www.youtube.com/@arguflow">
-            <BiLogosYoutube size={30} class="fill-current" />
-          </a>
-          <a
-            href="https://github.com/orgs/arguflow/repositories"
-            target="_blank"
-          >
-            <BiLogosGithub size={30} class="fill-current" />
-          </a>
-        </div>
       </footer>
     </div>
   );
